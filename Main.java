57cbbd5b4e685a453b3afc472a6ea68f21917f22
s25535 <s25535@pjwstk.edu.pl>
2022-06-16 16:46:12 +0200

    15.Próby generatora labiryntu

--- Main.java
+++ Main.java
@@ -14,10 +14,12 @@ public class Main {
         Random random = new Random();
 
         //random.nextInt(max - min + 1) + min max = 29 min = 1, rogi planszy nie mogą być startem
-        int start = random.nextInt(SIZE - 2)+1;
+        int start = random.nextInt(SIZE - 2) + 1;
+
         //Wyznaczenie pozycji startowej
-        int[][] pozycja = new int[0][start];
-        System.out.println("Start z pozycji x = "+start+", y = 0");
+        int y = 0;
+        int x = start;
+        System.out.println("Start z pozycji x = " + start + ", y = 0");
 
 
         JFrame frame = new JFrame();
@@ -25,12 +27,12 @@ public class Main {
         frame.add(panel);
         JButton[][] buttons = new JButton[SIZE][SIZE];
 
-        //Tworzenie Planszy z cellsami; x to kolumny, y to wiersze
-        for (int x = 0; x < SIZE; x++) {
-            for (int y = 0; y < SIZE; y++) {
+        //Tworzenie Planszy z cellsami; i to kolumny, j to wiersze
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 JButton cell = new JButton();
-                buttons[x][y] = cell;
-                buttons[x][y].addActionListener(e -> cell.setBackground(Color.BLACK));
+                buttons[i][j] = cell;
+                buttons[i][j].addActionListener(e -> cell.setBackground(Color.BLACK));
                 panel.add(cell);
             }
         }
@@ -41,33 +43,64 @@ public class Main {
 
 
         //Tworzenie ścian planszy
-        for(int i = 0; i <SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) {
             buttons[i][0].setBackground((Color.BLACK));
             cellBlock[i][0] = 1;
-            buttons[i][SIZE-1].setBackground((Color.BLACK));
-            cellBlock[i][SIZE-1] = 1;
+            buttons[i][SIZE - 1].setBackground((Color.BLACK));
+            cellBlock[i][SIZE - 1] = 1;
             buttons[0][i].setBackground((Color.BLACK));
             cellBlock[0][i] = 1;
-            buttons[SIZE-1][i].setBackground((Color.BLACK));
-            cellBlock[SIZE-1][i] = 1;
+            buttons[SIZE - 1][i].setBackground((Color.BLACK));
+            cellBlock[SIZE - 1][i] = 1;
         }
 
         //Pokoloruj start
         buttons[0][start].setBackground((Color.YELLOW));
+        y += 1;
 
-        //cellBlock[]
+        while(true) {
+            int liczba = 1;
+            switch (liczba) {
 
-
-
-
-        for (int x = 0; x < 30; x++) {
-            for (int y = 0; y < 30; y++) {
-                if(cellBlock[x][y] == 0){
-                    buttons[x][y].setBackground((Color.GRAY));
-                }
-                Thread.sleep(10);
+                case 1:
+                    if (cellBlock[y + 1][x] == 0) {
+                        buttons[y + 1][x].setBackground((Color.pink));
+                        cellBlock[y + 1][x] = 1;
+                        y += 1;
+                    }
+                    break;
+                case 2:
+                    if (cellBlock[y - 1][x] == 0) {
+                        buttons[y - 1][x].setBackground((Color.pink));
+                        cellBlock[y - 1][x] = 1;
+                        y -= 1;
+                    }
+                    break;
+                case 3:
+                    if (cellBlock[y][x + 1] == 0) {
+                        buttons[y][x + 1].setBackground((Color.pink));
+                        cellBlock[y][x + 1] = 1;
+                        x += 1;
+                    }
+                    break;
+                case 4:
+                    if (cellBlock[y][x - 1] == 0) {
+                        buttons[y][x - 1].setBackground((Color.pink));
+                        cellBlock[y][x - 1] = 1;
+                        x -= 1;
+                    }
+                    break;
             }
         }
+
+//        for (int i = 0; i< 30; i++) {
+//            for (int j= 0; j < 30; j++) {
+//                if(cellBlock[i][j] == 0){
+//                    buttons[i][j].setBackground((Color.GRAY));
+//                }
+//                Thread.sleep(10);
+//            }
+//        }
     }
 }
 
