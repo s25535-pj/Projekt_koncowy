9dd7e6966d3ba99519bbb1dcc9309af8608e90a5
s25535 <s25535@pjwstk.edu.pl>
2022-06-16 15:04:36 +0200

    Test 10 - kratka na planszy zmiena kolor po naciśnięciu myszą

--- Main.java
+++ Main.java
@@ -1,6 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class Main {
     public static void main(String[] args) {
 
-        new MyFrame();
+
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel(new GridLayout(30, 30));
+        frame.add(panel);
+
+        JButton[][] buttons = new JButton[30][30];
+
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 30; j++) {
+                JButton cell = new JButton();
+                buttons[i][j] = cell;
+                buttons[i][j].addActionListener(e -> cell.setBackground(Color.BLACK));
+                panel.add(cell);
+            }
+        }
+
+
+        frame.setSize(1000,1000);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
+
