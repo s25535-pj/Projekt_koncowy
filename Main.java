1165c484119c59e331f3fa5c5778662fb181ee77
s25535 <s25535@pjwstk.edu.pl>
2022-06-16 11:38:13 +0200

    Test 7 - nauka działania layout-a; użycie pętli

--- Main.java
+++ Main.java
@@ -6,18 +6,23 @@ public class Main {
         System.out.println("Dziala");
 
         JFrame frame = new JFrame();//okno
-        frame.setSize(500,500); //szerokosc, wysokosc okna
-        frame.setLayout(new GridLayout(3, 3));//menedżer ułożenia przycisków
+        frame.setSize(1000,1000); //szerokosc, wysokosc okna
+//        frame.setLayout(new GridLayout(3, 3));//menedżer ułożenia przycisków
+//
+//        frame.add(new JButton("1"));
+//        frame.add(new JButton("2"));
+//        frame.add(new JButton("3"));
+//        frame.add(new JButton("4"));
+//        frame.add(new JButton("5"));
+//        frame.add(new JButton("6"));
+//        frame.add(new JButton("7"));
+//        frame.add(new JButton("8"));
+//        frame.add(new JButton("9"));
 
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
+        frame.setLayout(new GridLayout(15,15));
+        for(int i = 0; i < 225; i++){
+            frame.add(new JButton("0"));
+        }
 
 
         frame.setVisible(true);//ramka ma być widoczna
