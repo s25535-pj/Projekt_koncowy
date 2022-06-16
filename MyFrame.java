4436928d1d6ca7b826824a12416231d1aebdc45e
s25535 <s25535@pjwstk.edu.pl>
2022-06-16 12:12:59 +0200

    Test 9 - Nauka button-a

--- MyFrame.java
+++ MyFrame.java
@@ -1,14 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame {
 
+    JButton button = new JButton();
     MyFrame(){
+
+        button.setBounds(450,450,100,100);
+        button.setText("przycisk");
+        button.setFocusable(false);
+        button.setBorder(BorderFactory.createBevelBorder(3));
+        button.addActionListener(e-> System.out.println("Działa"));
+        button.addActionListener(e-> button.setEnabled(false));
+        button.addActionListener(e-> button.setBackground(Color.BLACK));
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(1000,1000);
         this.setVisible(true);
 
+        this.add(button);
+
     }
+
 
 
 }
