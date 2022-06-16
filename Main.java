67bf8cf9cffe8374c029a7f526205032ae511fcb
s25535 <s25535@pjwstk.edu.pl>
2022-06-16 10:53:12 +0200

    Test 2 - dodanie jakiegokolwiek okna

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dziala");
+
+        JFrame frame = new JFrame();//ramka
+        JButton button = new JButton("Click!"); //przycisk
+        button.setBounds(130,100,100,40); //x, y, szerokość, wysokość
+
+        frame.add(button);
+
+        frame.setSize(400,500); //szerokosc, wysokosc
+        frame.setLayout(null);//bez menedżera "layoutu"
+        frame.setVisible(true);//ramka ma być widoczna
     }
 }
