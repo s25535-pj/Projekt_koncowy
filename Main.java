5a1c94925c24ffd5dd9ba9c3ca04e3e190fd3f86
s25535 <s25535@pjwstk.edu.pl>
2022-06-16 11:21:16 +0200

    Test 4 - nauka grida

--- Main.java
+++ Main.java
@@ -5,14 +5,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Dziala");
 
-        JFrame frame = new JFrame();//ramka
-        JButton button = new JButton("Click!"); //przycisk
-        button.setBounds(130,100,100,40); //x, y, szerokość, wysokość
+        JFrame frame = new JFrame();//okno
 
-        frame.add(button);
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+//        JButton button = new JButton("Click!"); //przycisk
+//        JButton button2 = new JButton("Pac!"); //przycisk2
+//        button.setBounds(130,100,100,40); //x, y, szerokość, wysokość
+//
+//        frame.add(button);
+//        frame.add(button2);
 
         frame.setSize(400,500); //szerokosc, wysokosc
-        frame.setLayout(new GridLayout(10, 10));//bez menedżera "layoutu"
+        frame.setLayout(new GridLayout(10, 10));//menedżer "layoutu"
         frame.setVisible(true);//ramka ma być widoczna
     }
 }
