e77197a6e7f2b375266a7520a8024a64d80c3be9
s25535 <s25535@pjwstk.edu.pl>
2022-06-17 16:21:36 +0200

    40. RUCH - kolejna próba innym sposobem

--- Game.java
+++ Game.java
@@ -2,38 +2,68 @@ import java.awt.Color;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Game extends JFrame implements KeyListener{
+public class Game extends FillTheBoard implements KeyListener {
 
-
+	JFrame frame = new JFrame();
 	JLabel label;
-	ImageIcon icon;
 
-	Game(){
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(500,500);
-		this.setLayout(null);
-		this.addKeyListener(this);
+	Game() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(0, 0);
+		frame.setLayout(null);
+		frame.addKeyListener(this);
 
-		icon = new ImageIcon("rocket.png");
-
-		label = new JLabel();
-		label.setBounds(0, 0, 100, 100);
-		label.setIcon(icon);
-		this.getContentPane().setBackground(Color.black);
-		this.add(label);
-		this.setVisible(true);
+		frame.getContentPane().setBackground(Color.black);
+		frame.setVisible(true);
+		x = start;
+		y = 0;
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		//keyTyped = Invoked when a key is typed. Uses KeyChar, char output
 		switch (e.getKeyChar()) {
-			case 'w' -> System.out.println('w');
-			case 's' -> System.out.println('s');
-			case 'd' -> System.out.println('d');
-			case 'a' -> System.out.println('a');
+			case 'w' -> moveUp();
+			case 's' -> moveDown();
+			case 'd' -> moveRight();
+			case 'a' -> moveLeft();
 		}
 
+	}
+	public void moveUp() {
+		System.out.println("w");
+		if (cellBlock[y][x] == 1) {
+			cellBlock[y][x] = 2;
+			buttons[y][x].setBackground(Color.YELLOW);
+			y -= 1;
+		}
+	}
+
+	public void moveDown() {
+		System.out.println("s");
+		if (cellBlock[y][x] == 1) {
+			System.out.println("działa");
+			cellBlock[y][x] = 2;
+			buttons[y][x].setBackground(Color.YELLOW);
+			y += 1;
+		}
+	}
+
+	public void moveRight() {
+		System.out.println("d");
+		if (cellBlock[y][x] == 1) {
+			cellBlock[y][x] = 2;
+			buttons[y][x].setBackground(Color.YELLOW);
+			x += 1;
+		}
+	}
+
+	public void moveLeft() {
+		System.out.println("a");
+		if (cellBlock[y][x] == 1) {
+			cellBlock[y][x] = 2;
+			buttons[y][x].setBackground(Color.YELLOW);
+			x -= 1;
+		}
 	}
 
 	@Override
@@ -44,40 +74,4 @@ public class Game extends JFrame implements KeyListener{
 	public void keyReleased(KeyEvent e) {
 	}
 }
-
-//	public void moveUp() {
-//		if (cellBlock[y][x] == 1) {
-//			System.out.println("w");
-//			cellBlock[y][x] = 2;
-//			buttons[y][x].setBackground(Color.YELLOW);
-//			y -= 1;
-//		}
-//	}
-
-//	public void moveDown() {
-//		if (cellBlock[y][x] == 1) {
-//			System.out.println("s");
-//			cellBlock[y][x] = 2;
-//			buttons[y][x].setBackground(Color.YELLOW);
-//			y += 1;
-//		}
-//	}
-//
-//	public void moveRight() {
-//		if (cellBlock[y][x] == 1) {
-//			System.out.println("d");
-//			cellBlock[y][x] = 2;
-//			buttons[y][x].setBackground(Color.YELLOW);
-//			x += 1;
-//		}
-//	}
-//
-//	public void moveLeft() {
-//		if (cellBlock[y][x] == 1) {
-//			System.out.println("a");
-//			cellBlock[y][x] = 2;
-//			buttons[y][x].setBackground(Color.YELLOW);
-//			x -= 1;
-//		}
-//	}
 
--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        FillTheBoard plansza = new FillTheBoard();
-//        plansza.createBoard();
-//        plansza.limitBoard();
-//        plansza.fillTheBoard();
+        FillTheBoard plansza = new FillTheBoard();
+        plansza.createBoard();
+        plansza.limitBoard();
+        plansza.fillTheBoard();
         Game game = new Game();
     }
 }
