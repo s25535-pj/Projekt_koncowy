a2c46ee4262c17eaa4cf36e6eb57bc317c8d7128
s25535 <s25535@pjwstk.edu.pl>
2022-06-17 11:42:54 +0200

    32. REFACTOR - powoli do przodu

--- Generator.java
+++ Generator.java
@@ -3,19 +3,32 @@ import java.awt.*;
 
 public class Generator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 		Generator generator = new Generator();
-        Generator.CreateMap();
+        Generator.createMap();
+        Generator.limitMap();
 	}
 
     static int SIZE = 31;
-//    int[][] cellBlock = new int[SIZE][SIZE];
+
+    static int[][] cellBlock = new int[SIZE][SIZE];
+
+    public int[][] getCellBlock() {
+        return cellBlock;
+    }
+    public static void setCellBlock(int x, int y, int value) {
+        cellBlock[y][x] = value;
+    }
+
+    static JButton[][] buttons = new JButton[SIZE][SIZE];
+
+
 //    int[][] deadEnd = new int[SIZE][SIZE];
 
-    public static void CreateMap(){
+    public static void createMap(){
         JFrame frame = new JFrame();
         JPanel panel = new JPanel(new GridLayout(SIZE, SIZE));
-        JButton[][] buttons = new JButton[SIZE][SIZE];
+//        JButton[][] buttons = new JButton[SIZE][SIZE];
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -30,6 +43,21 @@ public class Generator {
         frame.setSize(1000, 1000);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void limitMap() throws InterruptedException {
+        for (int i = 0; i < SIZE; i++) {
+            Thread.sleep(10);
+            buttons[i][0].setBackground((Color.BLACK));
+            setCellBlock(i,0,2);
+            buttons[i][SIZE - 1].setBackground((Color.BLACK));
+            setCellBlock(i,SIZE - 1, 2);
+            buttons[0][i].setBackground((Color.BLACK));
+            setCellBlock(0, i, 2);
+            buttons[SIZE - 1][i].setBackground((Color.BLACK));
+            setCellBlock(SIZE - 1, i, 2);
+        }
+
     }
 
 }
